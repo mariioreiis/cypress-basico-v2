0714ba4341c435687a4447d64341dfe8caad84c5
Mario Reis <mario@fabricadigital.com.br>
2022-05-17 20:00:32 -0300

    Método customizado e exercícios feitos

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="Cypress" />
-
 describe('Central de Atendimento ao Cliente TAT', function() {
+
+    const user = {}
     beforeEach(() => {
         cy.visit('./src/index.html')
       })
@@ -13,7 +14,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#firstName').type('Mario')
         cy.get('#lastName').type('Reis')
         cy.get('#email').type('mario@gmail.com')
-        cy.get('#open-text-area').type('Estou realizando um teste, preciso digitar algo longo para testar o delay do cypress Mussum Ipsum cacilds vidis litro abertis Mé faiz elementum girarzis  nisi eros vermeio Nullam volutpat risus nec leo commodo  ut interdum diam laoreet Sed non consequat odio Atirei o pau no gatis, per gatis num morreusA ordem dos tratores não altera o pão duris', {delay:0})
+        cy.get('#open-text-area').type('Estou realizando um teste, preciso digitar algo longo para testar o delay do cypress Mussum Ipsum cacilds vidis litro abertis Mé faiz elementum girarzis  nisi eros vermeio Nullam volutpat risus nec leo commodo  ut interdum diam laoreet Sed non consequat odio Atirei o pau no gatis, per gatis num morreusA ordem dos tratores não altera o pão duris')
         cy.get('.button').click()
         cy.get('.success').should('be.visible')
     }  
@@ -40,7 +41,6 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
-
         cy.get('#firstName').type('Mario')
         cy.get('#lastName').type('Reis')
         cy.get('#email').type('mario@gmail.com')
@@ -65,5 +65,22 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.button').click()
         cy.get('.error > strong').should('be.visible')
     })
+
+    it('envia o formulário com sucesso usando um comando customizado', function(){
+        cy.fillMandatoryFieldsAndSubmit()
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', function(){
+        cy.contains('Nome').type('Mario')
+        cy.contains('Sobrenome').type('Reis')
+        cy.contains('E-mail').type('mario@gmail.com')
+        cy.contains('Como podemos te ajudar?')
+        cy.get('#open-text-area').type('Estou realizando um teste')
+        cy.get('[type="submit"]').should('be.visible')
+        cy.contains('button','Enviar').click()
+        cy.contains('Mensagem enviada com sucesso.')
+     }  
+    )
+
   })
   
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,9 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
+    cy.get('#firstName').type('Mario')
+    cy.get('#lastName').type('Reis')
+    cy.get('#email').type('mario@gmail.com')
+    cy.get('#open-text-area').type('Aqui está sendo testado um input no campo de texto')
+    cy.get('.button').click()
+    cy.get('.success').should('be.visible')
+  })
+
